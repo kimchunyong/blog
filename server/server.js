f6a92a5bf42a2171cf46147e7972cd73cec8717a
chunyong <fr_lovemy5851>
2021-04-14 13:48:14 +0900

    feat : log 기능 추가

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 import app from './app';
+import { logger } from './config/winston';
 
 app.listen(7000, () => {
-    console.log('Server listening on port 3000');
+    logger.info('Server listening on port 3000');
+});
+
+app.get('/', (req, res) => {
+    logger.info('GET /');
+    res.sendStatus(200);
+});
+
+app.get('/error', (req, res) => {
+    logger.error('Error message');
+    res.sendStatus(500);
 });
