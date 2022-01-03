c3681d076fa869018f556fe358199628dc4228f4
chunyong <fr_lovemy5851>
2022-01-03 23:51:47 +0900

    feat:route settings

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,17 @@ import { ConnectedRouter } from "connected-react-router";
 import store, { history } from "./store";
 import MyRouter from "./routes/Router";
 
+import {BrowserRouter} from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
     return (
         <Provider store={store}>
             <ConnectedRouter history={history}>
-                <MyRouter />
+                <BrowserRouter>
+                    <MyRouter />
+                </BrowserRouter>
             </ConnectedRouter>
         </Provider>
     );
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {Container} from 'reactstrap';
 
 import AppNavbar from '../components/AppNav';
+import {Route, Switch,Redirect} from "react-router";
+import PostCardList from "./normalRoute/PostCardList";
+import PostWrite from "./normalRoute/PostWrite";
+import PostDetail from "./normalRoute/PostDetail";
+import Search from "./normalRoute/Search";
+import CategoryResult from "./normalRoute/CategoryResult";
 
 const MyRouter = () => {
     return (
         <>
             <AppNavbar/>
             <Header/>
-            <h1>Hello Body</h1>
+            <Container id="main-body">
+                <Switch>
+                    <Route path="/" exact component={PostCardList}/>
+                    <Route path="/post" exact component={PostWrite}/>
+                    <Route path="/post:id" exact component={PostDetail}/>
+                    <Route path="/post/category/:categoryName" exact component={CategoryResult}/>
+                    <Route path="/search/:searchTerm" exact component={Search}/>
+                    <Redirect from="*" to="/"/>
+                </Switch>
+            </Container>
             <Footer/>
         </>
     )
